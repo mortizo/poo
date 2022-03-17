ad414d58c2b10e225eec00e77ec50f8e133cd8ac
mortizo <mortizo@ups.edu.ec>
2022-03-17 16:26:13 -0500

    Unidad 01.01

---
+++ src/main/java/ups/edu/ec/poo/unidad01/semana1/Perro.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.poo.unidad01.semana1;
+
+/**
+ *
+ * @author Mauricio Ortiz
+ */
+
+
+public class Perro {
+    //Atributos
+    String nombre;
+    String raza;
+    int edad;
+    float peso;
+}
+
+
---
+++ src/main/java/ups/edu/ec/poo/unidad01/semana1/Persona.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ups.edu.ec.poo.unidad01.semana1;
+
+/**
+ *
+ * @author Mauricio Ortiz
+ */
+
+public class Persona {
+    String nombre;
+    short edad;
+    float peso;
+ 
+    String saludar(){
+        return "Hola Mundo";
+    }
+}
+
+
+
+
